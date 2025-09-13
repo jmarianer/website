7efa7d267a742a6dd86af04d79809287825f95d3
Joey Marianer <me@joeym.org>
2025-09-12 20:49:43 -0700

    Round 0 works, and so does advancing to the next round

--- boobtree/src/Game.tsx
+++ boobtree/src/Game.tsx
@@ -1,41 +1,54 @@
 import { useParams } from "react-router";
 import { database, useCurrentGame } from "./database";
 import { ref, set } from "firebase/database";
+import { useEffect } from "react";
 
 export function GameInProgress() {
-  const { started, archive, current_round } = useCurrentGame();
-  const { name: playerName, id } = useParams();
+  const { started, archive, current_round, players } = useCurrentGame();
+  const params = useParams();
+  const playerName = params.name!;
+  const id = params.id!;
+
+  useEffect(() => {
+    if (players.every((player) => player in archive[current_round])) {
+      set(ref(database, `boobtree/${id}/current_round`), current_round + 1);
+    }
+  }, [current_round, archive]);
 
   if (!started) {
     return <div>The game hasn't started yet. Please wait for the admin to start the game.</div>;
   }
 
-  if (playerName! in archive[current_round]) {
+  if (playerName in archive[current_round]) {
     return <div>Waiting for other players to finish round {current_round}...</div>;
   }
   const previousPlayer = useCurrentGame().previous_player[playerName!];
 
   if (current_round == 0) {
     return <>
-      <div>Write a phrase here</div>
-      <textarea id="next-phrase"></textarea>
+      <div id="instructions">Write a phrase here</div>
+      <div id="previous"></div>
+      <div id="next">
+        <textarea id="next-phrase"></textarea>
+      </div>
       <button id="done" onClick={() => {
-        console.log('dfas');
         set(ref(database, `boobtree/${id}/archive/${current_round}/${playerName}`), (document.getElementById('next-phrase') as HTMLTextAreaElement).value);
       }}>Done</button>
     </>;
   } else if (current_round % 2 == 1) {
     return <>
-      <div>Draw the phrase:</div>
-      <span id="previous-text">{archive[current_round-1][previousPlayer]}</span>
-      <div id="next-drawing">
+      <div id="instructions">Draw the phrase:</div>
+      <div id="previous">
+        <span>{archive[current_round - 1][previousPlayer]}</span>
+      </div>
+      <div id="next">
         <canvas id="next-drawing-canvas"></canvas>
       </div>
       <button id="done">Done</button>
     </>;
   } else {
     return <>
-      <div>Guess the phrase:</div>
+      <div id="instructions">Guess the phrase:</div>
       <div id="drawing-image">[image]</div>
       <input type="text" id="guess-input" />
       <button id="done">Done</button>
