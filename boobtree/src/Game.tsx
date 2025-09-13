39c37d611b3f03c4f8b6f1e0a5b382a4544930c3
Joey Marianer <me@joeym.org>
2025-09-12 21:23:30 -0700

    All rounds work (the archive doesn't).

--- boobtree/src/Game.tsx
+++ boobtree/src/Game.tsx
@@ -1,57 +1,117 @@
 import { useParams } from "react-router";
 import { database, useCurrentGame } from "./database";
 import { ref, set } from "firebase/database";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { Canvas, PencilBrush } from "fabric";
 
 export function GameInProgress() {
-  const { started, archive, current_round, players } = useCurrentGame();
+  const { started, archive, current_round, players, total_rounds } = useCurrentGame();
   const params = useParams();
   const playerName = params.name!;
   const id = params.id!;
 
   useEffect(() => {
+    if (current_round >= total_rounds) return;
     if (players.every((player) => player in archive[current_round])) {
       set(ref(database, `boobtree/${id}/current_round`), current_round + 1);
     }
   }, [current_round, archive]);
-
+  
   if (!started) {
     return <div>The game hasn't started yet. Please wait for the admin to start the game.</div>;
+  }
+  if (current_round >= total_rounds) {
+    return <div>The game is over! Here are the results:</div>;
   }
 
   if (playerName in archive[current_round]) {
     return <div>Waiting for other players to finish round {current_round}...</div>;
   }
-  const previousPlayer = useCurrentGame().previous_player[playerName!];
-
   if (current_round == 0) {
-    return <>
-      <div id="instructions">Write a phrase here</div>
-      <div id="previous"></div>
-      <div id="next">
-        <textarea id="next-phrase"></textarea>
-      </div>
-      <button id="done" onClick={() => {
-        set(ref(database, `boobtree/${id}/archive/${current_round}/${playerName}`), (document.getElementById('next-phrase') as HTMLTextAreaElement).value);
-      }}>Done</button>
-    </>;
+    return <FirstRound />;
   } else if (current_round % 2 == 1) {
-    return <>
-      <div id="instructions">Draw the phrase:</div>
-      <div id="previous">
-        <span>{archive[current_round - 1][previousPlayer]}</span>
-      </div>
-      <div id="next">
-        <canvas id="next-drawing-canvas"></canvas>
-      </div>
-      <button id="done">Done</button>
-    </>;
+    return <DrawingRound />;
   } else {
-    return <>
-      <div id="instructions">Guess the phrase:</div>
-      <div id="drawing-image">[image]</div>
-      <input type="text" id="guess-input" />
-      <button id="done">Done</button>
-    </>;
+    return <WritingRound />;
   }
+}
+
+function WritingRound() {
+  const { current_round, archive, previous_player } = useCurrentGame();
+  const params = useParams();
+  const playerName = params.name!;
+  const id = params.id!;
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  return <>
+    <div id="instructions">Describe that drawing below</div>
+    <div id="previous">
+      <img id="previous-drawing-image" src={archive[current_round - 1][previous_player[playerName]]} alt="Previous drawing" />
+    </div>
+    <div id="next">
+      <textarea id="next-phrase" ref={textareaRef}></textarea>
+    </div>
+    <button id="done" onClick={() => {
+      set(ref(database, `boobtree/${id}/archive/${current_round}/${playerName}`), textareaRef.current!.value);
+    } }>Done</button>
+  </>;
+}
+
+function DrawingRound() {
+  const { archive, current_round, previous_player } = useCurrentGame();
+  const params = useParams();
+  const playerName = params.name!;
+  const id = params.id!;
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricCanvasRef = useRef<Canvas>(null);
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = new Canvas(canvasRef.current, {
+      isDrawingMode: true,
+      width: 500,
+      height: 300,
+    });
+    canvas.freeDrawingBrush = new PencilBrush(canvas);
+    canvas.freeDrawingBrush.width = 5;
+    canvas.freeDrawingBrush.color = "#000000";
+
+    fabricCanvasRef.current = canvas;
+    return () => {
+      canvas.dispose();
+      fabricCanvasRef.current = null;
+    };
+  }, []);
+
+  return <>
+    <div id="instructions">Draw that phrase below</div>
+    <div id="previous">
+      <span>{archive[current_round - 1][previous_player[playerName]]}</span>
+    </div>
+    <div id="next">
+      <canvas ref={canvasRef} id="next-drawing-canvas"></canvas>
+    </div>
+    <button id="done" onClick={() => {
+      set(ref(database, `boobtree/${id}/archive/${current_round}/${playerName}`), fabricCanvasRef.current!.toDataURL({ format: 'png', multiplier: 1 }));
+    } }>Done</button>
+  </>;
+}
+
+function FirstRound() {
+  const { current_round } = useCurrentGame();
+  const params = useParams();
+  const playerName = params.name!;
+  const id = params.id!;
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  return <>
+    <div id="instructions">Write a phrase here</div>
+    <div id="previous"></div>
+    <div id="next">
+      <textarea id="next-phrase" ref={textareaRef}></textarea>
+    </div>
+    <button id="done" onClick={() => {
+      set(ref(database, `boobtree/${id}/archive/${current_round}/${playerName}`), textareaRef.current!.value);
+    } }>Done</button>
+  </>;
 }
