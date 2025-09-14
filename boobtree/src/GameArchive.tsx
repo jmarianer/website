a9dcb207a7eb089e597a131b877033d72e517e64
Joey Marianer <me@joeym.org>
2025-09-14 15:41:05 -0700

    Rename some stuff in archive and add "started/ended with"

--- boobtree/src/GameArchive.tsx
+++ boobtree/src/GameArchive.tsx
@@ -5,7 +5,7 @@ import "reveal.js/dist/theme/black.css";
 import { useCurrentGame } from "./database";
 
 export function GameArchive() {
-  const { archive, players } = useCurrentGame();
+  const { archive, players, total_rounds } = useCurrentGame();
 
   const deckDivRef = useRef<HTMLDivElement>(null); // reference to deck container div
   const deckRef = useRef<Reveal.Api | null>(null); // reference to deck reveal instance
@@ -31,35 +31,40 @@ export function GameArchive() {
   }, []);
 
   return (
-    // Your presentation is sized based on the width and height of
-    // our parent element. Make sure the parent is not 0-height.
     <div className="archive reveal" ref={deckDivRef}>
       <div className="slides">
-        {players.map((_, i) =>
-          <>
-            <section key={`player-${i}-title`}>
-              Chain {i+1}
+        {players.map((firstPlayer, chainNo) => {
+          const lastPlayer = players[(chainNo + total_rounds - 1) % players.length];
+          return <>
+            <section key={`chain-${chainNo}-title`}>
+              Chain {chainNo+1}
             </section>
-            {archive.map((round, j) => {
-              const player = players[(i + j) % players.length];
+            {archive.map((round, roundNo) => {
+              const player = players[(chainNo + roundNo) % players.length];
               return (
-                <section key={`player-${i}-round-${j}`}>
-                  {j % 2 === 1 ? (
+                <section key={`chainplayer-${chainNo}-round-${roundNo}`}>
+                  {roundNo % 2 === 1 ? (
                     <>
                       <div>{player} drew:</div>
-                      <img src={round[player]} alt={`Round ${j} drawing by ${player}`} />
+                      <img src={round[player]} alt={`Round ${roundNo} drawing by ${player}`} />
                     </>
-                  ) : (
+                  ) :
                     <>
                       <div>{player} wrote:</div>
-                      <div id="text">{round[player]}</div>
+                      <div className="text">{round[player]}</div>
                     </>
-                  )}
+                  }
                 </section>
               );
             })}
+            <section key={`chain-${chainNo}-end`}>
+              <div>Started with {firstPlayer}:</div>
+              <div className="text">{archive[0][firstPlayer]}</div>
+              <div>Ended with {lastPlayer}:</div>
+              <div className="text">{archive[total_rounds - 1][lastPlayer]}</div>
+            </section>
           </>
-        )}
+        })}
       </div>
     </div>
   );
